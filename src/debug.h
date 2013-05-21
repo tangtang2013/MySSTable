e0032bdeaa37f8634d866ffacf1e428a7d4c08ac
tangtang <761506092@qq.com>
2013-05-21 00:45:53 -0700

    * modify for linux make

--- src/debug.h
+++ src/debug.h
@@ -13,50 +13,97 @@ enum DEBUG_LEVEL {
 
 void __debug(char *file, int line,
 		enum DEBUG_LEVEL level, const char *fmt, ...);
+#ifdef WIN32
+	#ifdef INFO
+		#define __INFO(format, ...) \
+			do { __debug(__FILE__, __LINE__, \
+					LEVEL_INFO, format, __VA_ARGS__); \
+			} while (0)
+	#else
+		#define __INFO(format, args, ...) do {} while (0)
+	#endif
 
-#ifdef INFO
-	#define __INFO(format, ...) \
-		do { __debug(__FILE__, __LINE__, \
-				LEVEL_INFO, format, __VA_ARGS__); \
-		} while (0)
-#else
-	#define __INFO(format, args, ...) do {} while (0)
-#endif
+	#if defined(DEBUG) || defined(INFO)
+		#define __DEBUG(format, ...) \
+			do { __debug(__FILE__, __LINE__, \
+					LEVEL_DEBUG, \
+					format, __VA_ARGS__); \
+			} while (0)
+	#else
+		#define __DEBUG(format, args, ...) do {} while (0)
+	#endif
 
-#if defined(DEBUG) || defined(INFO)
-	#define __DEBUG(format, ...) \
-		do { __debug(__FILE__, __LINE__, \
-				LEVEL_DEBUG, \
+	#if defined(WARN) || defined(INFO)
+		#define __WARN(format, ...) \
+			do { __debug(__FILE__, __LINE__, \
+					LEVEL_WARNING,\
+					format, __VA_ARGS__); \
+			} while (0)
+	#else
+		#define __WARN(format, args, ...) do {} while (0)
+	#endif
+
+	#if defined(ERROR) || defined(DEBUG) || defined(WARN) || defined(INFO)
+	#define __ERROR(format, ...)\
+		do { __debug(__FILE__, __LINE__,\
+				LEVEL_ERROR,\
 				format, __VA_ARGS__); \
 		} while (0)
-#else
-	#define __DEBUG(format, args, ...) do {} while (0)
-#endif
+	#else
+		#define __ERROR(format, args, ...) do {} while (0)
+	#endif
 
-#if defined(WARN) || defined(INFO)
-	#define __WARN(format, ...) \
+	#define __PANIC(format, ...) \
 		do { __debug(__FILE__, __LINE__, \
-				LEVEL_WARNING,\
-				format, __VA_ARGS__); \
+				LEVEL_ERROR,\
+				format, __VA_ARGS__); abort(); exit(1); \
 		} while (0)
-#else
-	#define __WARN(format, args, ...) do {} while (0)
-#endif
 
-#if defined(ERROR) || defined(DEBUG) || defined(WARN) || defined(INFO)
-#define __ERROR(format, ...)\
-	do { __debug(__FILE__, __LINE__,\
-			LEVEL_ERROR,\
-			format, __VA_ARGS__); \
-	} while (0)
-#else
-	#define __ERROR(format, args, ...) do {} while (0)
-#endif
+	#endif
+#else	
+	//linux environment
+	#ifdef INFO
+	#define __INFO(format, args...) \
+		do { __debug(__FILE__, __LINE__, \
+		LEVEL_INFO, format, ##args); \
+		} while (0)
+	#else
+	#define __INFO(format, args...) do {} while (0)
+	#endif
 
-#define __PANIC(format, ...) \
-	do { __debug(__FILE__, __LINE__, \
-			LEVEL_ERROR,\
-			format, __VA_ARGS__); abort(); exit(1); \
-	} while (0)
+	#if defined(DEBUG) || defined(INFO)
+	#define __DEBUG(format, args...) \
+		do { __debug(__FILE__, __LINE__, \
+		LEVEL_DEBUG, \
+		format, ##args); \
+		} while (0)
+	#else
+	#define __DEBUG(format, args...) do {} while (0)
+	#endif
 
+	#if defined(WARN) || defined(INFO)
+	#define __WARN(format, args...) \
+		do { __debug(__FILE__, __LINE__, \
+		LEVEL_WARNING,\
+		format, ##args); \
+		} while (0)
+	#else
+	#define __WARN(format, args...) do {} while (0)
+	#endif
+
+	#if defined(ERROR) || defined(DEBUG) || defined(WARN) || defined(INFO)
+	#define __ERROR(format, args...)\
+		do { __debug(__FILE__, __LINE__,\
+		LEVEL_ERROR,\
+		format, ##args); \
+		} while (0)
+	#else
+	#define __ERROR(format, args...) do {} while (0)
+	#endif
+
+	#define __PANIC(format, args...) \
+		do { __debug(__FILE__, __LINE__, \
+		LEVEL_ERROR,\
+		format, ##args); abort(); exit(1); \
+		} while (0)
 #endif
