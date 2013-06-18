29e8e3dd021efed4e9882c0fa98c916504361d72
tangtang <761506092@qq.com>
2013-06-18 02:47:55 -0700

    * add some common libNet structure and function

--- src/common.h
+++ src/common.h
@@ -1,9 +1,45 @@
 #ifndef __SSTABLE_COMMON_H_
 #define __SSTABLE_COMMON_H_
 
-#include <Windows.h>
+#include <uv.h>
 
 #define FILE_MAX_PATH 260
+
+//*****************************  Net *******************************//
+
+//enum for massage type(socket)
+typedef enum msg_type
+{
+	CONNECTING,
+	CLOSECONNECT,
+	PUT_REQUEST,
+	PUT_REPLY,
+	GET_REQUEST,
+	GET_REPLY,
+	DEL
+}eMsgType;
+
+//write structure
+typedef struct {
+    uv_write_t req;
+    uv_buf_t buf;
+} write_req_t;
+
+//Server structure
+typedef struct ln_Server
+{
+//	uv_loop_t* loop;
+	uv_tcp_t server;
+	struct sockaddr_in bind_addr;
+
+	char strIp[32];
+	int	nPort;
+}stServer;
+
+//call back
+typedef write_req_t* (*MsgHandler_cb)(char* pInBuffer, int nInBufferSize);
+
+//*****************************  Net *******************************//
 
 typedef struct data_item
 {
@@ -17,6 +53,7 @@ typedef struct data_item
 	struct data_item* next;			//point 
 	char type;  // 's' and 'd':d is invalid data, s is valid data
 	unsigned long long version;		//data version
+	unsigned long long expireTime;
 	char addr[1];
 }data_t;
 
